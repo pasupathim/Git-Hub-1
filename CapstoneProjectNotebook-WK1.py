3cd8278cacff948e890c91e162f00431ad6cd749
pasupathi madheswaran <deekshna@pasupathis-MacBook-Pro-2.local>
2019-10-20 07:41:05 -0500

    first

---
+++ CapstoneProjectNotebook-WK1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #This is the python project for IBM Data Science capstone project
+
+# In[10]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[11]:
+
+
+print ("Hello Capstone Project Course!")
+
